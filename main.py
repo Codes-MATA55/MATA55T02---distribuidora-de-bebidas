a5e8e8bb831b36cafb961305311f493ab893446e
Samuel Marques Rodrigues de Miranda <nti-samuelmiranda@bahiana.edu.br>
2026-06-03 14:04:29 -0300

    feat(Samuel-Marques) modifica o main

---
+++ main.py
@@ -0,0 +1,27 @@
+class Usuario:
+    def __init__(self, id, nome, cargo):
+        self.id = id
+        self.nome = nome
+        self.cargo = cargo
+
+
+class Cargo:
+    def __init__(self, id, nome, funcao, salario):
+        self.id = id
+        self.nome = nome
+        self.funcao = funcao
+        self.salario = salario
+
+
+class Marca:
+    def __init__(self, id, nome):
+        self.id = id
+        self.nome = nome
+
+
+class Categoria:
+    def __init__(self, id, nome):
+        self.id = id
+        self.nome = nome
+
+
