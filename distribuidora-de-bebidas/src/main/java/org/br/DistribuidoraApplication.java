c6086a0ca3660bc584809fbaf9256bee4c633c74
Renato Marcelo <renatocelo189@gmail.com>
2026-06-03 22:01:05 -0300

    Criação do projeto base com DDD.

---
+++ distribuidora-de-bebidas/src/main/java/org/br/DistribuidoraApplication.java
@@ -0,0 +1,17 @@
+package org.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class DistribuidoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                DistribuidoraApplication.class,
+                args
+        );
+    }
+}
