4e031ec59ee6fd866f05e50acf328f033d175dd3
Bruno <soubruno@protonmail.com>
2026-07-13 18:05:18 -0300

    feat(bruno-fernandes): modelar categoria bebida

--- src/domain/enums/tipo_movimentacao.py
+++ src/domain/enums/tipo_movimentacao.py
@@ -4,3 +4,9 @@ from enum import Enum
 class MovementType(Enum):
     INBOUND = "ENTRADA"
     OUTBOUND = "SAÍDA"
+
+
+class BeverageCategory(Enum):
+    BEER = "CERVEJA"
+    SODA = "REFRIGERANTE"
+    JUICE = "SUCO"
